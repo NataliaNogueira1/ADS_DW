39ee0a5e486fa95f5cbd2a74ba1a1be8c33a663f
NataliaNogueira1 <natalianogueir2006@gmail.com>
2025-05-15 22:37:24 -0300

    Início aula 09

---
+++ Aula09-Bootstrap/bootstrap-projeto/script.js
@@ -0,0 +1,11 @@
+// Scroll Reveal
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".logoFonte", {});
+sr.reveal(".img-fluid", { origin: "right", delay: 400 });
